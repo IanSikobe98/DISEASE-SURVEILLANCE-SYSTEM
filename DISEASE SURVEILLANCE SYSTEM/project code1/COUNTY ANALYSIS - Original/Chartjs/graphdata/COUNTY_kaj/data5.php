b9e3d16450f6772555d4bc0b771a930079d936e7
IanSikobe98 <46993749+IanSikobe98@users.noreply.github.com>
2019-04-01 21:13:46 +0300

    Add files via upload

---
+++ DISEASE SURVEILLANCE SYSTEM/project code1/COUNTY ANALYSIS - Original/Chartjs/graphdata/COUNTY_kaj/data5.php
@@ -0,0 +1,41 @@
+<?php
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'disease_surveillance_database');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+	die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT  CName,Diarrhoea,Tuberculosis,Malnutrition,Ischemic_Disease,HIV,Malaria_pregnancy,General_malaria,Cardiovascular_Dis,Upper_Respiratory_Tract_Infections,Preterm_Babies  FROM total_cases WHERE County_Number = 34");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+ 
+// $myarray =array(json_encode($data));
+// print myarray;
+
+print json_encode($data);
