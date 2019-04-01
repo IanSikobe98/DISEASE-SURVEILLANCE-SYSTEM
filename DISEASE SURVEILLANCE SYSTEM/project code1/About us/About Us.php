93b4fad0566769aaf4f5a875755c80a433776b15
IanSikobe98 <46993749+IanSikobe98@users.noreply.github.com>
2019-04-01 20:51:03 +0300

    Add files via upload

---
+++ DISEASE SURVEILLANCE SYSTEM/project code1/About us/About Us.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ABOUT US</title>
+
+
+<link href="style.css" rel = "stylesheet" type = "text/css">
+
+<link href="css/bootstrap.css" rel = "stylesheet" type = "text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link href="js/bootstrap.js" type = "text/javascript">
+</head>
+<body>
+
+<div class ="header">
+
+<div class = "row">
+	<div class = "title">
+	<p class ="Heading"> &nbsp DISEASE SURVEILLANCE KENYA</p> 
+    </div>
+	<ul class ="main-nav">
+    <li class = "active"><a href="..\index.php">Home</a></li>
+    <li class ="litext"><a href="">services<i class="fa fa-sort-desc" style="font-size:10px"></i></a>
+    <ul> 
+    <li ><a href="..\Prediction select page\PRE-SEL.php">Disease Prediction</a></li>
+    <li ><a href="..\Disease Analysis selection\AN-SEL.php">Disease Analysis</a></li>
+    <li ><a href="..\Disease Deduction selection\DEDUC-SEL.php">Disease Deduction</a></li>
+    <li ><a href="..\Reports selection page\REP-SEL.php">Reports</a></li>
+    </ul> </li>
+    <li><a href="About us.php">about</a></li>
+    <li><a href="..\Contact Page\Contactpage.php">contact</a></li>
+    <li><a href="..\Login Page/login.php">Admin Login </a></li> 
+    
+</ul>
+</div>
+<div class = "motto">
+	<h1 class ="text">ABOUT US </h1>
+    <hr width="400px" align="right" color = "white" size = "40%" class="hrtitle">
+</div>
+</div>
+<div class = "body">
+<div class = "container">
+
+<h1 class = "text3">MAIN AIM</h1>
+  <p>In Kenya, about 58 % of the deaths that have occurred have been caused by the top ten main diseases namely HIV/AIDS, Lower Respiratory Infections, Diarrheal Diseases, Protein-Energy Malnutrition, Birth Asphyxia and Trauma, Stroke, Preterm Birth Complications, Malaria, TB and Ischemic Heart Disease and yet deaths from these diseases are preventable.<p>
+  <!-- h5 class="text4" --><p>Because of the above knowledge I decided that by the end of my project that I should create a system that can observe and analyze past data on previous occurrences of the diseases and to also use this data to predict the trends of how the diseases will spread which will help in preventing future epidemics and in the end effectively help minimize or even end the percentage of deaths caused by the major diseases in the country. The information from my system will help county governments to prepare their county well for an epidemic and even educate their respective counties on how to avoid such diseases during certain periods in a year so as to prevent any person from being affected from such diseases. </p>
+</div>
+</div>
+
+<div class ="Body2">
+<div class = "rou-img">
+<img src="ian.jpg" class="rounded-circle" alt="Cinque Terre" width="304" height="236"> 
+</div>
+<div class = "ian">
+<h1>Hi my name is Ian Walter Sikobe</h1>
+<h5>I am a founder , director and Project Manager at Disease Surveillance Kenya Limited. My role entails collection of data updating of data and i also designed this system that is widely used to help county governments to prevent epidemics of diseases.</h5>
+</div>
+<!-- <div class = "Body3">
+<div class = "title3">
+<p></p>
+<p></p> -->
+<!-- <h1> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OUR SERVICES </h1>
+
+</div>
+<div class ="Report">
+<img src="report.png" class = "rimage">
+<h2>REPORT GENERATION</h2>
+<p>This system will help generate reports for diseases affecting counties per month</p>
+</div>
+<div class ="Analysis">
+	<img src="data-analysis.jpg" class = "aimage">
+	<h2>&nbsp;DATA ANALYSIS</h2>
+<p>This system will also use existing data from DHS analyse the data and display analysis of the data </p>
+
+</div>
+<div class ="Prediction">
+	<img src="Prediction.png" class = "pimage">
+<h2>DATA PREDICTION</h2>
+<p>This system will use prediction models to predict future disease trends</p>
+
+</div>	
+<div class = "Deduction">
+	<img src="deduction.jpg" class = "pimage">
+<h2>DATA DEDUCTION</h2>
+<p>This system will use existing data to deduce  &nbsp;the  socio-economic causes of diseases which can be controlled</p>
+
+</div> -->
+
+
+
+</div>
+<div class="Footer-top">
+	
+
+</div>
+
+<div class = "footer-middle">
+<div class = "Quick">
+<h1 class = "quicklinksnm"><u>Quick links</u></h1>
+<hr size = "10"width="50%" align="center" color = "#42C0FB" >
+<ul>
+    <li><a href ="../index.php">Home</a></li>
+    <li ><a href="../Prediction select page\PRE-SEL.php">Disease Prediction</a></li>
+    <li ><a href="../Disease Analysis selection\AN-SEL.php">Disease Analysis</a></li>
+    <li ><a href="../Disease Deduction selection\DEDUC-SEL.php">Disease Deduction</a></li>
+    <li ><a href="../Reports selection page\REP-SEL.php">Reports</a></li>
+    <li><a href ="../Login Page/login.php">Admin Login</a></li>
+</ul>
+</div>
+<div class ="contactus">
+<h1 class = "contact">Contacts</h1>	
+<hr width="50%" align="center" color = "white" size = "40%">
+<ul>
+<li><a href ="#">Mobile : 0721383105</a></li>
+<li><a href ="#">Email :ianwalter309@gmail.com</a></li>
+</ul>
+</div>
+
+
+<div class = "footer-bottom">
+
+<p class="foottext">&copy; 2018 Disease Surveillance Management System Services | Site Credits | Privacy Policy</p>
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
