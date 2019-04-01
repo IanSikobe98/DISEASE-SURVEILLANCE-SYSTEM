de9b638cf7b593f33063f5a871ac795bfebe2f94
IanSikobe98 <46993749+IanSikobe98@users.noreply.github.com>
2019-04-02 00:13:04 +0300

    Add files via upload

---
+++ DISEASE SURVEILLANCE SYSTEM/project code1/Disease Deduction selection/DEDUC-SEL.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DEDUCTION COUNTY SELECT PAGE</title>
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+<link href="style.css" rel = "stylesheet" type = "text/css">
+
+<link href="css/bootstrap.css" rel = "stylesheet" type = "text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link href="js/bootstrap.js" type = "text/javascript">
+</head>
+<body>
+
+
+
+<div class ="header"class="col-sm-4">
+
+<div class = "row"class="col-sm-4">
+	<div class = "title" class="col-sm-4">
+	<p class ="Heading"class="col-sm-4">DISEASE SURVEILLANCE KENYA</p> 
+    </div>
+   
+    <div class = "Sidebar"class="col-sm-4">
+
+    <ul class ="main-nav">
+
+
+  <li class = "active"><a href="..\index.php">Home</a></li>
+    <li class ="litext"><a href="">services <i class="fa fa-sort-desc" style="font-size:10px"></i></a>
+    <ul> 
+    <li ><a href="..\Prediction select page\PRE-SEL.php">Disease Prediction</a></li>
+    <li ><a href="..\Disease Analysis selection\AN-SEL.php">Disease Analysis</a></li>
+    <li ><a href="DEDUC-SEL.php">Disease Deduction</a></li>
+    <li ><a href="..\Reports selection page\REP-SEL.php">Reports</a></li>
+    </ul> </li>
+    <li><a href="..\About us\About Us.php">about</a></li>
+    <li><a href="..\Contact Page\Contactpage.php">contact</a></li>
+    <li><a href="..\Login Page/login.php">Admin Login </a></li>   
+    
+</ul>
+
+   
+    </div>
+
+<div class = "lBody"class="col-sm-4">
+<div class = "logindi"class="col-sm-4">
+
+<div class="logintitle"class="col-sm-4">
+
+</div>
+<div class="cont">
+  <h2 class= "formti">County Deduction Selection form</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="county" class ="lab1">County:</label>
+    <select class="form-control" id="sel1" placeholder="Enter county" name="county">
+       <option value = "..\COUNTY DEDUCTION\NAIROBI COUNTY DEDUCTION.PHP">Nairobi</option>
+        <option value = "..\COUNTY DEDUCTION\KWALE COUNTY DEDUCTION.PHP">Kwale</option>
+        <option value = "..\COUNTY DEDUCTION\MOMBASA COUNTY DEDUCTION.PHP">Mombasa</option>
+       <!--  <option value = "..\COUNTY DEDUCTION\MIGORI COUNTY DEDUCTION.PHP">Migori</option>  -->   
+       <!--  <option value =  "..\COUNTY DEDUCTION\BARINGO COUNTY DEDUCTION.PHP">Baringo</option>   --> 
+        <option value =  "..\COUNTY DEDUCTION\BUSIA COUNTY DEDUCTION.PHP">Busia</option>
+      <!--   <option value =  "..\COUNTY DEDUCTION\MERU COUNTY DEDUCTION.PHP">Meru</option> -->
+        <option value =  "..\COUNTY DEDUCTION\VIHIGA COUNTY DEDUCTION.PHP">Vihiga</option>
+        <!-- <option value = "..\COUNTY DEDUCTION\KAJIADO COUNTY DEDUCTION.PHP" >Kajiado</option> -->
+        <option value =  "..\COUNTY DEDUCTION\MAKUENI COUNTY DEDUCTION.PHP">Makueni</option> 
+    
+   </select>
+</div>
+    <button type="submit" class="btn btn-default">County Deduction</button>
+  </form>
+</div>
+
+
+
+</div>
+
+
+</div>
+</div>
+<div class="Footer-top"class="col-sm-4">
+	
+
+</div>
+
+<div class = "footer-middle"class="col-sm-4">
+<div class = "Quick"class="col-sm-4">
+<h1 class = "quicklinksnm"class="col-sm-4"><u>Quick links</u></h1>
+<hr size = "10"width="50%" align="center" color = "#42C0FB" >
+<ul>
+    <li><a href ="../index.php">Home</a></li>
+    <li ><a href="../Prediction select page\PRE-SEL.php">Disease Prediction</a></li>
+    <li ><a href="../Disease Analysis selection\AN-SEL.php">Disease Analysis</a></li>
+    <li ><a href="DEDUC-SEL.php">Disease Deduction</a></li>
+    <li ><a href="../Reports selection page\REP-SEL.php">Reports</a></li>
+    <li><a href ="../Login Page/login.php">Admin Login</a></li>
+</ul>
+</div>
+<div class ="contactus" class="col-sm-4">
+<h1 class = "contact" class="col-sm-4">Contacts</h1>	
+<hr width="50%" align="center" color = "white" size = "40%">
+<ul>
+<li><a href ="#">Mobile : 0721383105</a></li>
+<li><a href ="#">Email :ianwalter309@gmail.com</a></li>
+</ul>
+</div>
+
+
+<div class = "footer-bottom" class="col-sm-4">
+
+<p class="foottext">&copy; 2018 Disease Surveillance Management System Services | Site Credits | Privacy Policy</p>
+
+</div>
+</div>
+
+
+
+
+
+<script type="text/javascript" src="selection.js"></script>
+
+
+</body>
+
+</html>
