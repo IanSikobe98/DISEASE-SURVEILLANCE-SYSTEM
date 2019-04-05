896ad591ddc46b6558173bfdc70c2bd2dada1f9a
IanSikobe98 <46993749+IanSikobe98@users.noreply.github.com>
2019-04-05 22:14:48 +0300

    Add files via upload

---
+++ DISEASE SURVEILLANCE SYSTEM/project code1/index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="style.css" rel = "stylesheet" type = "text/css">
+
+<link href="css/bootstrap.css" rel = "stylesheet" type = "text/css">
+
+
+<link href="js/bootstrap.js" type = "text/javascript">
+</head>
+<body>
+
+<div class ="header">
+
+<div class = "row">
+	<div class = "title">
+	<p class ="Heading"> &nbsp DISEASE SURVEILLANCE KENYA</p> 
+    </div>
+	<ul class ="main-nav">
+	<li class = "active"><a href="index.php">Home</a></li>
+    <li class ="litext"><a href="">services<i class="fa fa-sort-desc" style="font-size:10px"></i></a>
+    <ul> 
+    <li ><a href="Prediction select page\PRE-SEL.php">Disease Prediction</a></li>
+    <li ><a href="Disease Analysis selection\AN-SEL.php">Disease Analysis</a></li>
+    <li ><a href="Disease Deduction selection\DEDUC-SEL.php">Disease Deduction</a></li>
+    <li ><a href="Reports selection page\REP-SEL.php">Reports</a></li>
+    </ul> </li>
+    <li><a href="About us\About Us.php">about</a></li>
+    <li><a href="Contact Page\Contactpage.php">contact</a></li>
+    <li><a href="Login Page/login.php">Admin Login </a></li>    
+    
+</ul>
+</div>
+<div class = "motto">
+	<h1 class ="text">Together we can eradicate these disesases</h1>
+</div>
+</div>
+<div class = "body">
+<div class = "container">
+
+<h1 class = "text3">WHO ARE WE</h1>
+  <h5 class="text4">It is with my great pleasure that i welcome you to the Disease Surveillance website which is part and integral of our system functionality.</h5>
+  <h5 class="text4">The website will help showcase the fundamentals of he system stating from the website to the rest.</h5>
+  <h5 class = "text4">The system will  focus on a sample of 6 of the 47 counties.The counties to be looked into include:Mombasa,Kwale,Busia,Nairobi,Vihiga and Makueni</h5>
+</div>
+</div>
+
+<div class ="Body2">
+
+<h1 class ="text5 "><i>"Prevention is better than cure" is as true today in health as it was when Franklin made the quote. </i></h1>
+</div>
+<div class = "Body3">
+<div class = "title3">
+<p></p>
+<p></p>
+<h1> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OUR SERVICES </h1>
+
+</div>
+<div class ="Report" class = "col-sm-3">
+<img src="report.png" class = "rimage">
+<h2 >REPORT GENERATION</h2>
+<p>This system will help generate reports for diseases affecting counties per month</p>
+</div>
+<div class ="Analysis" class = "col-sm-3">
+	<img src="data-analysis.jpg" class = "aimage">
+	<h2>&nbsp;DATA ANALYSIS</h2>
+<p>This system will also use existing data from DHS analyse the data and display analysis of the data </p>
+
+</div>
+<div class ="Prediction" class = "col-sm-3">
+	<img src="Prediction.png" class = "pimage">
+<h2>DATA PREDICTION</h2>
+<p>This system will use prediction models to predict future disease trends</p>
+
+</div>	
+<div class = "Deduction" class = "col-sm-3">
+	<img src="deduction.jpg" class = "pimage">
+<h2>DATA DEDUCTION</h2>
+<p>This system will use existing data to deduce  &nbsp;the  socio-economic causes of diseases which can be controlled</p>
+
+</div>
+
+
+
+</div>
+<div class="Footer-top">
+	
+
+</div>
+
+<div class = "footer-middle">
+<div class = "Quick">
+<h1 class = "quicklinksnm"><u>Quick links</u></h1>
+<hr size = "10"width="50%" align="center" color = "#42C0FB" >
+<ul>
+    <li><a href ="index.php">Home</a></li>
+    <li ><a href="Prediction select page\PRE-SEL.php">Disease Prediction</a></li>
+    <li ><a href="Disease Analysis selection\AN-SEL.php">Disease Analysis</a></li>
+    <li ><a href="Disease Deduction selection\DEDUC-SEL.php">Disease Deduction</a></li>
+    <li ><a href="Reports selection page\REP-SEL.php">Reports</a></li>
+    <li><a href ="Login Page/login.php">Admin Login</a></li>
+</ul>
+</div>
+<div class ="contactus">
+<h1 class = "contact">Contacts</h1>	
+<hr width="50%" align="center" color = "white" size = "40%">
+<ul>
+<li><a href ="#">Mobile : 0721383105</a></li>
+<li><a href ="#">Email :ianwalter309@gmail.com</a></li>
+</ul>
+</div>
+
+
+<div class = "footer-bottom">
+
+<p class="foottext">&copy; 2018 Disease Surveillance Management System Services | Site Credits | Privacy Policy</p>
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
